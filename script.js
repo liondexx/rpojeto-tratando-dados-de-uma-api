35d50bc8615986facb2e20c0747a695d64cbb930
lion patrocinio <lionfilesoffices@gmail.com>
2022-08-18 15:38:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var mostraCep = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse cep não existe!');
+    }console.log(r)
+})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('processamento concluido'));
+
+
+
